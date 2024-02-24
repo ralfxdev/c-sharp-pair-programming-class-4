0f3df9b4c8f893ce580ee84aa7eb0484ccc28473
ralfxdev <lopezalexander1019@gmail.com>
2024-02-24 11:36:10 -0600

    add programs

---
+++ first-five-pair-numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿for (int i = 1; i < 10; i++)
+{
+    try
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ups, hubo un error: " + ex.Message);
+    }
+}
---
+++ passwordValidate/Program.cs
@@ -0,0 +1,17 @@
+﻿String pass = "jutiapa";
+String userPass = "";
+
+do
+{
+    try
+    {
+        Console.WriteLine("Ingrese la contraseña: ");
+        userPass = Console.ReadLine()!;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Ups, hubo un error: " + ex.Message);
+    }
+} while (userPass != pass);
+
+Console.WriteLine($"Bienvenido al sistema!");
